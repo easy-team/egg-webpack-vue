455c2f267ba5d85e45dfdd7367459eb90afe3bf1
hubcarl <hubcarl@126.com>
2017-11-16 15:16:10 +0800

    refactor:egg-view-vue

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 'use strict';
 const path = require('path');
 const co = require('co');
-const Manifest = require('webpack-manifest-normalize');
 module.exports = app => {
   if (app.view) {
     app.view.resolve = function* (name) {
@@ -10,8 +9,9 @@ module.exports = app => {
   }
 
   if (app.vue) {
-    const renderBundle = app.vue.renderBundle;
-    app.vue.renderBundle = (name, context, options) => {
+    const render = app.vue.render;
+    app.vue.bundleCache = false;
+    app.vue.render = (name, context, options) => {
       const filePath = path.isAbsolute(name) ? name : path.join(app.config.view.root[0], name);
       const promise = app.webpack.fileSystem.readWebpackMemoryFile(filePath, name);
       return co(function* () {
@@ -19,21 +19,8 @@ module.exports = app => {
         if (!content) {
           throw new Error(`read webpack memory file[${filePath}] content is empty, please check if the file exists`);
         }
-        return renderBundle.bind(app.vue)(content, context, options);
+        return render.bind(app.vue)(content, context, options);
       });
     };
   }
-
-  app.messenger.on(app.webpack.Constant.EVENT_WEBPACK_BUILD_STATE, data => {
-    if (data.state) {
-      const filepath = app.config.webpackvue.manifest;
-      const promise = app.webpack.fileSystem.readWebpackMemoryFile(filepath);
-      promise.then(content => {
-        if (content) {
-          Manifest.saveFile(filepath, content);
-          app.messenger.sendToApp('webpack_manifest_save_success', { state: true });
-        }
-      });
-    }
-  });
 };
--- config/config.default.js
+++ config/config.default.js
@@ -1,9 +1,5 @@
 'use strict';
 const path = require('path');
-module.exports = app => {
-  const config = {};
-  config.webpackvue = {
-    manifest: path.join(app.baseDir, 'config/manifest.json'),
-  };
-  return config;
+module.exports = () => {
+  return {};
 };
